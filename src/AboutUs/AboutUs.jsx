80676711fad415ead06d6c22ea91dcb106246684
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2025-03-14 19:44:10 -0400

    cosmetic changes

--- src/AboutUs/AboutUs.jsx
+++ src/AboutUs/AboutUs.jsx
@@ -4,9 +4,16 @@ import Navbar from '../Navbar';
 
 function AboutUs() {
   return (
-    <div className="aboutUs-page"> 
-      <Navbar/>
+    <>
+    <Navbar/>
+    <div className="row aboutUs-page"> 
+      <div className="col-2 left-border"></div>
+      <div className="col-8 content">
+      <h1 className='heading'>About Us</h1>
+      </div>
+      <div className="col-2 right-border"></div>
     </div>
+    </>
   );
 }
 
