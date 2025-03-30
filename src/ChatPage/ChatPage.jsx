d96e726160b7b3515f5cf35b21a12bad16f37b38
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2025-03-29 21:25:31 -0400

    Updated the chatbot page

--- src/ChatPage/ChatPage.jsx
+++ src/ChatPage/ChatPage.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './ChatPage.css';
 import Navbar from '../Navbar';
+import Chatbot from "react-chatbot-kit";
+import config from '../chatbot/config';
+import MessageParser from '../chatbot/MessageParser';
+import ActionProvider from '../chatbot/ActionProvider';
+import 'react-chatbot-kit/build/main.css';
 
 function ChatPage() {
   return (
-    <div className="chat-page"> 
+    <div className="chat-page">
       <Navbar/>
+      <div className="chatbot-container">
+        <Chatbot
+          config={config}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+        />
+      </div>
     </div>
   );
 }
