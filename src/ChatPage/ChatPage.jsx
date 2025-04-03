46773f7c9251ea11c3ddba90b1767da4a6a20347
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2025-04-03 19:10:21 -0400

    frontend manges

--- src/ChatPage/ChatPage.jsx
+++ src/ChatPage/ChatPage.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ChatPage.css';
 import Navbar from '../Navbar';
-import Chatbot from "react-chatbot-kit";
-import config from '../chatbot/config';
-import MessageParser from '../chatbot/MessageParser';
-import ActionProvider from '../chatbot/ActionProvider';
-import 'react-chatbot-kit/build/main.css';
 
 function ChatPage() {
+  const [dialogList, setDialogList] = useState([]);
+
+  const addChatbotMsg = (msg) => {
+    setDialogList([...dialogList, { user: 'Cinebot', msg, alignment: 'left' }]);
+  };
+
+  const addUserMsg = (msg) => {
+    setDialogList([...dialogList, { user: 'User', msg, alignment: 'right' }]);
+  };
+
   return (
     <div className="chat-page">
-      <Navbar/>
-      <div className="chatbot-container">
-        <Chatbot
-          config={config}
-          messageParser={MessageParser}
-          actionProvider={ActionProvider}
-        />
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="col-1">
+          </div>
+          <div className="col-10">
+            <div className="chatbot-container">
+              {dialogList.map((dialog, index) => (
+                <div className={`card ${dialog.alignment === 'left' ? 'card-left' : 'card-right'}`} key={index}>
+                  <div className="card-body">
+                    <h5 className="card-title">{dialog.user}</h5>
+                    <p className="card-text">{dialog.msg}</p>
+                  </div>
+                </div>
+              ))}
+              <button onClick={() => addChatbotMsg('Hello, I am Cinebot!')}>Add Chatbot Message</button>
+              <button onClick={() => addUserMsg('Hi Cinebot, I am Ronumau')}>Add User Message</button>
+            </div>
+          </div>
+          <div className="col-1">
+          </div>
+        </div>
       </div>
     </div>
   );
