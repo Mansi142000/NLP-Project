e7777297ec45cbae46c78fdc148d8339b8b623ed
Rohan Rajendra Dalvi <rohan@xander.tech>
2025-04-10 19:15:37 -0400

    Update index.jsx

--- src/ChatPage/ChatBox/ChatDialogue/index.jsx
+++ src/ChatPage/ChatBox/ChatDialogue/index.jsx
@@ -5,13 +5,13 @@ import './../../ChatPage.css';
 
 function ChatDialogue({dialog, index}) {
   if (dialog.role === 'system') return null; // Skip rendering
-
+  const displayRole = dialog.role === 'user' ? 'User' : 'Cine-Bot';
   const alignment = dialog.role === 'user' ? 'card-right' : 'card-left';
 
   return (
     <div className={`card ${alignment}`} key={index}>
         <div className="card-body">
-            <h5 className="card-title">{dialog.role}</h5>
+            <h5 className="card-title">{displayRole}</h5>
             <p className="card-text">{dialog.content}</p>
         </div>
     </div>
