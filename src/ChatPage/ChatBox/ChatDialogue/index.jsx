ce62fadcdb22a68afd94a3d79e29bb8d060f7cc4
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2025-04-16 00:00:30 -0400

    commit

--- src/ChatPage/ChatBox/ChatDialogue/index.jsx
+++ src/ChatPage/ChatBox/ChatDialogue/index.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import './index.css'; // Import the CSS for styling
 import './../../ChatPage.css';
 
-
-function ChatDialogue({dialog, index}) {
+function ChatDialogue({ dialog, index }) {
   if (dialog.role === 'system') return null; // Skip rendering
 
   const alignment = dialog.role === 'user' ? 'card-right' : 'card-left';
 
+  // Set the card title based on the role
+  const cardTitle = dialog.role === 'assistant' ? 'Cinebot' : 'User';
+
   return (
     <div className={`card ${alignment}`} key={index}>
-        <div className="card-body">
-            <h5 className="card-title">{dialog.role}</h5>
-            <p className="card-text">{dialog.content}</p>
-        </div>
+      <div className="card-body">
+        <h5 className="card-title">{cardTitle}</h5>
+        <p className="card-text">{dialog.content}</p>
+      </div>
     </div>
   );
 }
